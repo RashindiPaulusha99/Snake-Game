393462ba1664f41e5ec2ff4e120334f81604db69
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-04-05 14:45:10 +0530

    add game over alert

--- SnakeGame/assets/js/script.js
+++ SnakeGame/assets/js/script.js
@@ -8,31 +8,82 @@ var snake=[
     {offsetX:50, offsetY:40,oldX:0, oldY:0},
 ];
 
+var food = {offsetX:100, offsetY:100}
+
 var snakeWidth = 10;
 var snakeHeight = 10;
 
 var gap = 10;
+var score = 0;
 
 var keyPress;
 
 createSnake();
 /*visible snake*/
-function createSnake() {
+function createSnake(index, value) {
 
     for (var i = 0; i < snake.length; i++) {
         ctx.fillStyle = 'red';
         ctx.fillRect(snake[i].offsetX, snake[i].offsetY, snakeWidth, snakeHeight);
         ctx.strokeStyle='white';
         ctx.strokeRect(snake[i].offsetX, snake[i].offsetY, snakeWidth, snakeHeight);
+
+        /*if (snake[i] == 0) {
+            if (collided(snake[i].x, snake[i].y)) {
+                console.log("over");
+            }
+        }*/
+
     }
+
+
 }
 
-setInterval(move,500);
-var stop = setInterval(moveDown,500);
+function collided(x,y) {
+    return snake.filter(function (value,index) {
+        return index != 0 && value.x == x && value.y == y;
+    }).length > 0 || x < 0 || x > canvas.width || y < 0 || y > canvas.height;
+}
+
+setInterval(move,400);
+var stop = setInterval(moveDown,400);
 
 function move() {
     clearCanvas();
+    createFood();
     snakeMove();
+    //createSnake();
+}
+
+function createFood() {
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(food.offsetX, food.offsetY,snakeWidth, snakeHeight);
+
+    /*increase score and eat food and snake makes bigger*/
+    if (snake[0].offsetX == food.offsetX && snake[0].offsetY == food.offsetY){
+
+        score++;
+        $("#score").text(score);
+
+        snake.push({
+            offsetX: snake[snake.length-1].oldX,
+            offsetY: snake[snake.length-1].oldY
+        });
+
+        generateNewPosition();
+    }
+
+    for (var i in snake) {
+
+
+        if (i != 0){
+            if (snake[0].offsetX == snake[i].offsetX && snake[0].offsetY == snake[i].offsetY || snake[0].offsetY === canvas.height || snake[0].offsetX === canvas.width || snake[0].offsetY === 0 || snake[0].offsetX === 0 ){
+                alert("game over");
+                clearInterval(stop);
+                return;
+            }
+        }
+    }
 }
 
 /*running down at the beginning*/
@@ -45,6 +96,7 @@ function snakeMove() {
 
     for (var i = 0; i <snake.length ; i++) {
 
+        ctx.fillStyle = 'red';
         ctx.fillRect(snake[i].offsetX, snake[i].offsetY, snakeWidth, snakeHeight);
         ctx.strokeRect(snake[i].offsetX, snake[i].offsetY, snakeWidth, snakeHeight);
 
@@ -69,14 +121,49 @@ function snakeMove() {
            snake[i].offsetX = snake[i - 1].oldX;
            snake[i].offsetY = snake[i - 1].oldY;
        }
+
     }
 }
 
+/*what's the pressed key*/
 $(document).keydown(function (e) {
     keyPress = e.key;
 });
 
+/*clear previous box of snake when it move*/
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function generateNewPosition() {
+    var randX = Math.floor(Math.random() * (canvas.width - 10));
+    var randY = Math.floor(Math.random() * (canvas.height - 10));
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(randX, randY,snakeWidth, snakeHeight);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+}
+
+/*function collided(x,y) {
+    return snake.filter(function (value,index) {
+        return index != 0 && value.x == x && value.y == y;
+    }).length > 0 || x < 0 || x > canvas.width || y < 0 || y > canvas.height;
+}*/
+
+function gameOver(index,x,y) {
+    if (index != 0){
+        if (snake[0].offsetX == snake[index].offsetX && snake[0].offsetY == snake[index].offsetY){
+            alert("game over");
+
+        }
+    }
+}
+
+$("#canvas").keydown(function (event) {
+    console.log("x",event.pageX);
+    console.log("y",event.pageY);
+    console.log(canvas.height);
+});
+
+
 
